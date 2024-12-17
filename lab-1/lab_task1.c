0fc821e8895d11f11c501d51ef4e642e575a37d9
rcsi-dev <tbboneez@gmail.com>
2024-12-17 17:08:44 +0300

    Fixed code and recompiled

--- lab-1/lab_task1.c
+++ lab-1/lab_task1.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-void modificate(int ram, int lat) {
-    ram = ram - 1;
-    lat = lat + 1;
+void modificate(int *ram, int *lat) {
+    *ram = *ram - 1;
+    *lat = *lat + 1;
 }
 
 void main() {
     int ram = 32;
     int latency = 64;
 
-    modificate(ram, latency);
+    modificate(&ram, &latency);
     printf("%i %i\n", ram, latency);
 }
+
+// код не работал как нужно, так как в функции передавались копии значений
+// и изменялись соответственно тоже копии, теперь мы передаем адреса переменных
+// и изменяем оригинальные переменные через указатели
