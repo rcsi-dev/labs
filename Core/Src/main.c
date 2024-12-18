f915e535e00a1c29ca9aeaa4df7ade8b572445d9
rcsi-dev <tbboneez@gmail.com>
2024-12-18 22:12:23 +0300

    Completed 3rd task -- Edited readme

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,28 +3,34 @@
 
 void delay(uint32_t count) {
     while (count--) {
-        // выполняем команду no operations вместо пустого цикла,
-        // чтобы избежать проблем при компиляции
-        __NOP();
+        __NOP();  // выполняем команду no operations
     }
 }
 
 int main(void) {
-    // включаем тактирование GPIO
-	// записывая в регистр AHB1ENR (отвечает за тактирование перифирии шины AHB1)
-	// бит, включающий тактирование порта GPIOA (т.к. светодиод подключен к PA5)
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    // включаем тактирование GPIOA и GPIOC
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
 
-    // настраиваем PA5 на выход
+    // настроим PA5 как выход для светодиода
     GPIOA->MODER &= ~(GPIO_MODER_MODER5_Msk);  // сбрасываем битовое поле MODER5
-    GPIOA->MODER |= (1U << GPIO_MODER_MODER5_Pos); // устанавливаем MODER5 в режим Output (01)
+    GPIOA->MODER |= (1U << GPIO_MODER_MODER5_Pos); // устанавливаем PA5 в режим Output
 
-    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5);  // устанавливаем тип выхода на push-pull
-    GPIOA->OSPEEDR |= (0U << GPIO_OSPEEDR_OSPEED5_Pos);  // низкая скорость переключения пина
-    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD5_Msk);  // отключение подтяжки во избежание ненужной нагрузки
+    GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5);  // push-pull
+    GPIOA->OSPEEDR |= (0U << GPIO_OSPEEDR_OSPEED5_Pos);  // низкая скорость
+    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD5_Msk);  // без подтяжки
+
+    // настроим PC13 как вход с подтяжкой
+    GPIOC->MODER &= ~(GPIO_MODER_MODER13_Msk);  // режим входа для PC13
+    GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPD13_Msk);  // сбрасываем, чтобы установить подтяжку
+    GPIOC->PUPDR |= GPIO_PUPDR_PUPD13_0;  // подтяжка вверх
 
     while (1) {
-        GPIOA->ODR ^= GPIO_ODR_OD5;  // инверсия состояния пина PA5
-        delay(1000000);
+        // чтение состояния кнопки на PC13
+        if ((GPIOC->IDR & GPIO_IDR_ID13) == 0) {  // кнопка нажата (активен низкий уровень)
+            GPIOA->ODR |= GPIO_ODR_OD5;  // включаем светодиод (PA5 high)
+        } else {
+            GPIOA->ODR &= ~GPIO_ODR_OD5;  // выключаем светодиод (PA5 low)
+        }
+        delay(100000);  // небольшой таймаут, чтобы избежать дребезга кнопки
     }
 }
